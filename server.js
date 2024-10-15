1444aea01f6c2b028a990016dc6b1393718a7b4c
Patrick Cuba <cubap@slu.edu>
2024-10-14 23:35:49 -0500

    semicolons

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-import { configDotenv } from 'dotenv';
-configDotenv();
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import connectDB from './config/db.js';
+import { configDotenv } from 'dotenv'
+configDotenv()
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import connectDB from './config/db.js'
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+const app = express()
+const server = createServer(app)
+const io = new Server(server)
 
 // Middleware
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Connect to MongoDB
-connectDB();
+connectDB()
 
 // Basic route
 app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+  res.send('Server is running')
+})
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
 
-export { app };
+export { app }
