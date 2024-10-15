3fe57f6122f1532305669a95794b7cae0675e9f2
Patrick Cuba <cubap@slu.edu>
2024-10-15 00:03:40 -0500

    db work

--- client.js
+++ client.js
@@ -1,12 +1,18 @@
+import 'dotenv/config'
 import { io } from 'socket.io-client'
+import mongoose from 'mongoose'
 
 const socket = io('http://localhost:5000')
+
+// Use a valid ObjectId
+const validObjectId = process.env.TEST_PAWN
+// const validObjectId = new mongoose.Types.ObjectId().toString()
 
 socket.on('connect', () => {
   console.log('Connected to server')
 
-  // Example player action
-  socket.emit('playerAction', { entityId: 'exampleEntityId', newPosition: { x: 10, y: 20 } })
+  // Example player action with a valid ObjectId
+  socket.emit('playerAction', { entityId: validObjectId, newPosition: { x: 10, y: 20 } })
 
   // Listen for game state updates
   socket.on('gameState', (state) => {
