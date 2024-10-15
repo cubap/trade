6b97d4edb7d3639833f1d0b00028953b918a5eff
Patrick Cuba <cubap@slu.edu>
2024-10-15 00:30:43 -0500

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -1,35 +1,31 @@
+import mongoose from 'mongoose'
 import { Server } from 'socket.io'
 import Entity from './models/Entity.js'
-import Feature from './models/Feature.js'
 
 const setupGameLogic = (server) => {
   const io = new Server(server)
 
-  const simulateWorld = async () => {
-    // Example: Move entities, update features
-    const entities = await Entity.find()
-    for (const entity of entities) {
-      // Example logic to update entity state
-      entity.attributes.energy -= 1
-      await entity.save()
-    }
-    setTimeout(simulateWorld, 1000) // Simulate every second
-  }
-
   io.on('connection', (socket) => {
     console.log('New player connected')
 
-    // Handle player actions
     socket.on('playerAction', async (action) => {
-      const entity = await Entity.findById(action.entityId)
-      if (entity) {
-        // Process player action (example: move entity)
-        entity.position = action.newPosition
-        await entity.save()
+      console.log(`Received action: ${JSON.stringify(action)}`)
 
-        // Broadcast updated game state
-        io.emit('gameState', { entities: await Entity.find() })
+      if (!mongoose.Types.ObjectId.isValid(action.entityId)) {
+        console.log(`Invalid entity ID: ${action.entityId}`)
+        return
       }
+
+      const entity = await Entity.findById(action.entityId)
+      if (!entity) {
+        console.log(`Entity not found for ID: ${action.entityId}`)
+        return
+      }
+
+      console.log(`Entity found: ${entity}`)
+      entity.position = action.newPosition
+      await entity.save()
+      io.emit('gameState', { entities: await Entity.find() })
     })
 
     socket.on('disconnect', () => {
@@ -37,8 +33,16 @@ const setupGameLogic = (server) => {
     })
   })
 
-  // Start the world simulation
   simulateWorld()
+}
+
+const simulateWorld = async () => {
+  const entities = await Entity.find()
+  for (const entity of entities) {
+    entity.attributes.energy -= 1
+    await entity.save()
+  }
+  setTimeout(simulateWorld, 1000)
 }
 
 export default setupGameLogic
