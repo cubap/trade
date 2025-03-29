494141c85740cf984fc7478084410608f4f4e45e
Patrick Cuba <cubap@slu.edu>
2025-03-28 21:31:05 -0500

    big world

--- solo/js/app.js
+++ solo/js/app.js
@@ -3,14 +3,16 @@ import World from './core/World.js'
 import CanvasRenderer from './rendering/CanvasRenderer.js'
 import setupControls from './ui/controls.js'
 
-// Create the world and renderer
-const world = new World(800, 600)
+// Create a larger world
+const world = new World(2000, 2000)
 const renderer = new CanvasRenderer(world, 'game-canvas')
 
-// Setup initial entities
+// Add more initial entities spread throughout the larger world
 world.addEntity(new Entity('Entity1', 100, 100, 20))
-world.addEntity(new Entity('Entity2', 200, 200, 30))
-world.addEntity(new Entity('Entity3', 300, 150, 15))
+world.addEntity(new Entity('Entity2', 500, 800, 30))
+world.addEntity(new Entity('Entity3', 1200, 400, 15))
+world.addEntity(new Entity('Entity4', 1500, 1300, 25))
+world.addEntity(new Entity('Entity5', 800, 1800, 18))
 
 // Setup UI controls
 const controls = setupControls(world, renderer)
